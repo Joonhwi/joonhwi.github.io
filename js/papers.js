e0bfcc5c3d93c6099da7fcde33b59ff39e37f0e9
Joonhwi <joonhwia@gmail.com>
2024-03-12 20:56:42 -0700

    Update papers.js

--- js/papers.js
+++ js/papers.js
@@ -5,84 +5,84 @@ var papers = [
     arxivnumber: "2403.01837",
     released: 1
   },
-  {
-    title: "Worldline QFT with Phase Space Actions",
-    codename: "feynmanbracket",
-    arxivnumber: "24**.*****",
-    released: -1
-  },
-  {
-    title: "Quaternionic Approach to 6d Three-Point Amplitudes",
-    codename: "quaternionic",
-    arxivnumber: "****.*****",
-    released: -1
-  },
-  {
-    title: "Covariantly Conserved Quantities of Kerr Probe up to All Orders in Spin",
-    codename: "ambikerr-yano",
-    arxivnumber: "24**.*****",
-    released: -1
-  },
-  {
-    title: "Twistors for Binaries",
-    codename: "binary-twistor",
-    arxivnumber: "240*.*****",
-    released: -1
-  },
-  {
-    title: "Spinspacetime for Binaries",
-    codename: "binary-sst",
-    arxivnumber: "240*.*****",
-    released: -1
-  },
-  {
-    title: "Covariant Symplectic Perturbation Theory in Massive Twistor Space",
-    codename: "ambikerr2",
-    arxivnumber: "2308.*****",
-    released: 0
-  },
-  {
-    title: "Covariant Symplectic Perturbation Theory",
-    codename: "spt",
-    arxivnumber: "2308.*****",
-    released: 0
-  },
-  {
-    title: "Symplectic Perturbation Theory",
-    codename: "spt",
-    arxivnumber: "2308.*****",
-    released: 0
-  },
-  {
-    title: "Non-Canonical Poisson Brackets Revisited",
-    codename: "feynmanbracket",
-    arxivnumber: "2308.*****",
-    released: 0
-  },
-  {
-    title: "Zig-Zag Form of 1PM Spinning Binary Hamiltonian",
-    codename: "binary-1pm",
-    arxivnumber: "2308.*****",
-    released: 0
-  },
-  {
-    title: "Foundations of Curved Spinspacetime: Earth",
-    codename: "sst-earth",
-    arxivnumber: "2308.*****",
-    released: 0
-  },
-  {
-    title: "The Kerr Action from Orbit-Spin Duality",
-    codename: "sodual",
-    arxivnumber: "2307.*****",
-    released: 0
-  },
-  {
-    title: "Gilbert-Ampère Duality",
-    codename: "ga",
-    arxivnumber: "2307.*****",
-    released: 0
-  },
+  // {
+  //   title: "Worldline QFT with Phase Space Actions",
+  //   codename: "feynmanbracket",
+  //   arxivnumber: "24**.*****",
+  //   released: -1
+  // },
+  // {
+  //   title: "Quaternionic Approach to 6d Three-Point Amplitudes",
+  //   codename: "quaternionic",
+  //   arxivnumber: "****.*****",
+  //   released: -1
+  // },
+  // {
+  //   title: "Covariantly Conserved Quantities of Kerr Probe up to All Orders in Spin",
+  //   codename: "ambikerr-yano",
+  //   arxivnumber: "24**.*****",
+  //   released: -1
+  // },
+  // {
+  //   title: "Twistors for Binaries",
+  //   codename: "binary-twistor",
+  //   arxivnumber: "240*.*****",
+  //   released: -1
+  // },
+  // {
+  //   title: "Spinspacetime for Binaries",
+  //   codename: "binary-sst",
+  //   arxivnumber: "240*.*****",
+  //   released: -1
+  // },
+  // {
+  //   title: "Covariant Symplectic Perturbation Theory in Massive Twistor Space",
+  //   codename: "ambikerr2",
+  //   arxivnumber: "2308.*****",
+  //   released: 0
+  // },
+  // {
+  //   title: "Covariant Symplectic Perturbation Theory",
+  //   codename: "spt",
+  //   arxivnumber: "2308.*****",
+  //   released: 0
+  // },
+  // {
+  //   title: "Symplectic Perturbation Theory",
+  //   codename: "spt",
+  //   arxivnumber: "2308.*****",
+  //   released: 0
+  // },
+  // {
+  //   title: "Non-Canonical Poisson Brackets Revisited",
+  //   codename: "feynmanbracket",
+  //   arxivnumber: "2308.*****",
+  //   released: 0
+  // },
+  // {
+  //   title: "Zig-Zag Form of 1PM Spinning Binary Hamiltonian",
+  //   codename: "binary-1pm",
+  //   arxivnumber: "2308.*****",
+  //   released: 0
+  // },
+  // {
+  //   title: "Foundations of Curved Spinspacetime: Earth",
+  //   codename: "sst-earth",
+  //   arxivnumber: "2308.*****",
+  //   released: 0
+  // },
+  // {
+  //   title: "The Kerr Action from Orbit-Spin Duality",
+  //   codename: "sodual",
+  //   arxivnumber: "2307.*****",
+  //   released: 0
+  // },
+  // {
+  //   title: "Gilbert-Ampère Duality",
+  //   codename: "ga",
+  //   arxivnumber: "2307.*****",
+  //   released: 0
+  // },
   {
     title: "Asymptotic Spinspacetime",
     codename: "sst-asym",
