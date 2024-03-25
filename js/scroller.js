8a18f207374e6d1e0ae9587b036881015d013eb6
Joonhwi <joonhwia@gmail.com>
2024-03-24 20:27:25 -0700

    Update scroller.js

--- js/scroller.js
+++ js/scroller.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
   const images = [
-    "/images/phys-cards/lcdx.png",
+    // "/images/phys-cards/lcdx.png",
     "https://media.gcflearnfree.org/ctassets/topics/246/share_size_large.jpg",
     "/images/phys-cards/test.jpg",
     "https://media.gcflearnfree.org/ctassets/topics/246/share_size_large.jpg",
