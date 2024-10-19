1695b8c4719fdc7fc4141acdae8eb5a1481ee51c
Joonhwi <joonhwia@gmail.com>
2024-10-18 17:45:15 -0700

    Update scroller.js

--- js/scroller.js
+++ js/scroller.js
@@ -26,66 +26,6 @@ document.addEventListener('DOMContentLoaded', function() {
       // + ` realizes Ezra Ted Newman's dream of unifying spacetime and spin into a complex geometry and opens up a new chapter in relativity.`
       + ` opens up a new chapter in relativity, by realizing Ezra Ted Newman's dream of unifying spacetime and spin into a complex geometry.`
       + ` [`
-        + getArxivLink(2) + `,`
-        + getArxivLink(3)
-      + `]`
-    ,
-      // `<b><i>Zig-zag theory of massive spinning particles</i></b>`
-      `<span class="EmphBI">Zig-zag twistor particle program</span>`
-      // + ` aims to reboot twistor particle programme with an emphasis on the hidden Kähler geometry (“dubbed zig-zag structure”) of massive spinning particles.`
-      // + ` aims to reboot twistor particle programme with an emphasis on the hidden Kähler geometry of massive spinning particles in four dimensions [1].`
-      // + ` aims to reboot the twistor particle program with an emphasis on the hidden Kähler geometry of massive spinning particles in four dimensions.`
-      + ` aims to reboot the twistor particle program, with an emphasis on the Kähler geometry of the massive twistor space.`
-      + ` [`
-        + getArxivLink(1) + `,`
-        + getArxivLink(2) + `,`
-        + getArxivLink(3) + `,`
-        + getArxivLink(6)
-      + `]`
-    ,
-      // `<span class="EmphBI">Zig-zag program for spinning black holes</span>`
-      `<span class="EmphBI">Black hole zig-zag program</span>`
-      // + `proposes to study spinning black holes in four dimensions from the angle of their factorization into chiral dyons in the chiral sectors.`
-      // + `proposes to study spinning black holes in four dimensions with their “factorized” form, into chiral dyons in chiral sectors.`
-      // + `proposes to study the dynamics of spinning black holes in four dimensions with their factorization into chiral dyons.`
-      // + ` proposes that chiral dyons (Taub-NUT instantons, dubbed “zig” and “zag”) should be used as elementary building blocks for studying the dynamics of spinning black holes in four dimensions.`
-      // + ` proposes that self-dual and anti-self-dual Taub-NUT instantons (dubbed “zig” and “zag”) should be used as “atoms” for studying the physics of spinning black holes in four dimensions.`
-      + ` understands the physics of spinning black holes in four dimensions by using self-dual and anti-self-dual Taub-NUT instantons (dubbed “zig” and “zag”) as elementary building blocks (or “atoms”).`
-      // + ` [4]`
-      + ` [`
-        + getArxivLink(3) + `,`
-        + getArxivLink(5)
-    + `]`
-    ,
-      `<span class="EmphBI">Color-Kinematics Duality</span>`
-      // + ` suggests that there exists a mystery infinite-dimensional Lie algebra at the heart of perturbative quantum gravity, somehow different than the diffeomorphism algebra.`
-      // + ` suggests that there exists a mystery infinite-dimensional Lie algebra at the heart of perturbative quantum gravity, which somehow isn't the diffeomorphism algebra.`
-      // + ` suggests that there exists a mystery infinite-dimensional Lie algebra at the heart of perturbative quantum gravity, which somehow isn't simply the diffeomorphism algebra in spacetime.`
-      + ` suggests that there lies a mystery infinite-dimensional Lie algebra at the heart of perturbative quantum gravity.`
-      // + ` suggests that gravitons are gluons with a mystery infinite-dimensional Lie algebra.`
-      + ` `
-    ,
-      `<span class="EmphBI">Generalized Symmetry in Gravity</span>`
-      // `<b><i>Higher-Form Symmetry in Gravity</i></b>`
-      // + `proposes to study spinning black holes in four dimensions from the angle of their factorization into chiral dyons in the chiral sectors.`
-      // + `proposes to study spinning black holes in four dimensions with their “factorized” form, into chiral dyons in chiral sectors.`
-      // + `proposes to study the dynamics of spinning black holes in four dimensions with their factorization into chiral dyons.`
-      // + ` reveals new symmetry in nature and explores a novel interpretation of gravitational singularities as topological operators.`
-      // + ` reveals new symmetry in nature that could explain why fermions exist and also a novel interpretation of gravitational singularities as topological operators.`
-      // + ` has taught us why fermions exist in our universe and revealed a novel interpretation of gravitational singularities as topological operators.`
-      // + ` has shed an interesting insight on why fermions exist and revealed a novel interpretation of gravitational singularities as topological operators.`
-      // + ` reveals a new symmetry in our universe below the scale of the lightest neutrino and also illuminates a novel interpretation of gravitational singularities as topological operators.`
-      // + ` offers an explanation of why fermions exist in our universe and reveals a novel interpretation of gravitational singularities as topological operators.`
-      // + ` has offered an interesting angle on why fermions exist and revealed an intriguing interpretation of gravitational singularities as topological operators.`
-      // + ` reveals an intriguing interpretation of gravitational singularities as topological operators while also offering new insights on the standard model.`
-      // + ` reinterprets gravitational singularities as topological operators and sheds new insights on the standard model.`
-      // + ` offers an interesting reinterpretation of gravitational singularities as topological operators and sheds an intriguing insight on how fermions exist in our universe.`
-      // + ` offers an interesting reinterpretation of gravitational singularities as topological operators and reveals a new symmetry in the standard model.`
-      + ` reveals a new symmetry in the standard model while offering an interesting reinterpretation of gravitational singularities as topological operators.`
-      // + ` [3]`
-      + ` [`
-        + getArxivLink(4)
-    + `]`
     ,
   ];
 
